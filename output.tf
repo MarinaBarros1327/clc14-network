86148cbab23d7a2d954e0c4c4c71c6c5edc80be0
Marina Barros <marinabarros1327@gmail.com>
2025-12-04 20:07:36 +0000

    update output

--- output.tf
+++ output.tf
@@ -1,58 +1,54 @@
-###################################
-# VPC
-###################################
 output "vpc_id" {
-  value = aws_vpc.minha_vpc.id
+  description = "ID da VPC criada"
+  value       = aws_vpc.minha_vpc.id
 }
 
-###################################
-# Subnets
-###################################
-output "subnet_publica_1a" {
-  value = aws_subnet.public_subnet_1a.id
-}
-
-output "subnet_publica_1b" {
-  value = aws_subnet.public_subnet_1b.id
-}
-
-output "subnet_privada_1a" {
-  value = aws_subnet.private_subnet_1a.id
-}
-
-output "subnet_privada_1b" {
-  value = aws_subnet.private_subnet_1b.id
-}
-
-###################################
-# Route Tables
-###################################
-output "route_table_public" {
-  value = aws_route_table.public_rt.id
-}
-
-output "route_table_private_1a" {
-  value = aws_route_table.priv_rt_1a.id
-}
-
-output "route_table_private_1b" {
-  value = aws_route_table.priv_rt_1b.id
-}
-
-###################################
-# NAT Gateways
-###################################
-output "nat_gateway_1a" {
-  value = aws_nat_gateway.nat_gw_1a.id
-}
-
-output "nat_gateway_1b" {
-  value = aws_nat_gateway.nat_gw_1b.id
-}
-
-###################################
-# Internet Gateway
-###################################
 output "internet_gateway_id" {
-  value = aws_internet_gateway.igw.id
+  description = "Internet Gateway da VPC"
+  value       = aws_internet_gateway.igw.id
+}
+
+output "nat_gateway_1a_id" {
+  description = "NAT Gateway da zona us-east-1a"
+  value       = aws_nat_gateway.nat_gw_1a.id
+}
+
+output "nat_gateway_1b_id" {
+  description = "NAT Gateway da zona us-east-1b"
+  value       = aws_nat_gateway.nat_gw_1b.id
+}
+
+output "subnet_publica_1a_id" {
+  description = "Subnet pública da zona 1a"
+  value       = aws_subnet.public_subnet_1a.id
+}
+
+output "subnet_publica_1b_id" {
+  description = "Subnet pública da zona 1b"
+  value       = aws_subnet.public_subnet_1b.id
+}
+
+output "subnet_privada_1a_id" {
+  description = "Subnet privada da zona 1a"
+  value       = aws_subnet.private_subnet_1a.id
+}
+
+output "subnet_privada_1b_id" {
+  description = "Subnet privada da zona 1b"
+  value       = aws_subnet.private_subnet_1b.id
+}
+
+output "route_table_public_id" {
+  description = "Tabela de rota pública"
+  value       = aws_route_table.public_rt.id
+}
+
+output "route_table_private_1a_id" {
+  description = "Tabela de rota privada da zona 1a"
+  value       = aws_route_table.priv_rt_1a.id
+}
+
+output "route_table_private_1b_id" {
+  description = "Tabela de rota privada da zona 1b"
+  value       = aws_route_table.priv_rt_1b.id
 }
