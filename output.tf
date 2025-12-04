9d548663a6af4f4fb99f830e23e06173feecb65d
Marina Barros <marinabarros1327@gmail.com>
2025-12-04 20:12:40 +0000

    update exibir output

--- output.tf
+++ output.tf
@@ -1,54 +1,78 @@
+##############################################
+# VPC
+##############################################
+
 output "vpc_id" {
-  description = "ID da VPC criada"
+  description = "ID da VPC criada pelo Terraform."
   value       = aws_vpc.minha_vpc.id
 }
 
+##############################################
+# Internet Gateway
+##############################################
+
 output "internet_gateway_id" {
-  description = "Internet Gateway da VPC"
+  description = "ID do Internet Gateway associado à VPC."
   value       = aws_internet_gateway.igw.id
 }
 
+##############################################
+# NAT Gateways
+##############################################
+
 output "nat_gateway_1a_id" {
-  description = "NAT Gateway da zona us-east-1a"
+  description = "ID do NAT Gateway criado na zona us-east-1a."
   value       = aws_nat_gateway.nat_gw_1a.id
 }
 
 output "nat_gateway_1b_id" {
-  description = "NAT Gateway da zona us-east-1b"
+  description = "ID do NAT Gateway criado na zona us-east-1b."
   value       = aws_nat_gateway.nat_gw_1b.id
 }
 
+##############################################
+# Subnets Públicas
+##############################################
+
 output "subnet_publica_1a_id" {
-  description = "Subnet pública da zona 1a"
+  description = "ID da subnet pública localizada em us-east-1a."
   value       = aws_subnet.public_subnet_1a.id
 }
 
 output "subnet_publica_1b_id" {
-  description = "Subnet pública da zona 1b"
+  description = "ID da subnet pública localizada em us-east-1b."
   value       = aws_subnet.public_subnet_1b.id
 }
 
+##############################################
+# Subnets Privadas
+##############################################
+
 output "subnet_privada_1a_id" {
-  description = "Subnet privada da zona 1a"
+  description = "ID da subnet privada localizada em us-east-1a."
   value       = aws_subnet.private_subnet_1a.id
 }
 
 output "subnet_privada_1b_id" {
-  description = "Subnet privada da zona 1b"
+  description = "ID da subnet privada localizada em us-east-1b."
   value       = aws_subnet.private_subnet_1b.id
 }
 
+##############################################
+# Route Tables
+##############################################
+
 output "route_table_public_id" {
-  description = "Tabela de rota pública"
+  description = "ID da tabela de rota pública associada ao Internet Gateway."
   value       = aws_route_table.public_rt.id
 }
 
 output "route_table_private_1a_id" {
-  description = "Tabela de rota privada da zona 1a"
+  description = "ID da tabela de rota privada da zona us-east-1a."
   value       = aws_route_table.priv_rt_1a.id
 }
 
 output "route_table_private_1b_id" {
-  description = "Tabela de rota privada da zona 1b"
+  description = "ID da tabela de rota privada da zona us-east-1b."
   value       = aws_route_table.priv_rt_1b.id
 }
